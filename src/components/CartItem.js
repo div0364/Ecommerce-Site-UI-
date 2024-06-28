758f82ec9d154a054512cbe35e80ad4e7de0593b
div0364 <divyam.d.sinha@gmail.com>
2024-06-28 11:06:15 +0530

    update cart page

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,38 +1,35 @@
 import React from "react";
-import {FcDeleteDatabase} from "react-icons/fc"
+import { FcDeleteDatabase } from "react-icons/fc";
 import { useDispatch } from "react-redux";
-import {toast} from "react-hot-toast"
-import {remove} from "../redux/Slices/CartSlice"
+import { toast } from "react-hot-toast";
+import { remove } from "../redux/Slices/CartSlice";
+import "./CartItem.css";
 
-const CartItem=({item,itemIndex}) =>{
-const dispatch =useDispatch();
+const CartItem = ({ item, itemIndex }) => {
+  const dispatch = useDispatch();
 
-const removeFromCart=()=>{
+  const removeFromCart = () => {
     dispatch(remove(item.id));
-    toast.success("Item is removed"); 
-}
-    return (
-        <div>
-            <div>
-                <div>
-                    <img src={item.image}/>
+    toast.success("Item is removed");
+  };
 
-                    </div>
-                    <div>
-                        <h1>{item.title}</h1>
-                        <h1>{item.description}</h1>
-                        <div>
-                            <p>{item.price}</p>
-                            <div 
-                            onClick={removeFromCart}
-                            >
-                                <FcDeleteDatabase/>
-                            </div>
-                        </div>
-                    </div>
-            </div>
+  return (
+    <div className="cart-item">
+      <div>
+        <img src={item.image} alt={item.title} />
+      </div>
+      <div className="cart-item-content">
+        <h1 className="cart-item-title">{item.title}</h1>
+        <p className="cart-item-description">{item.description}</p>
+        <div className="cart-item-footer">
+          <p className="cart-item-price">${item.price}</p>
+          <div className="cart-item-remove" onClick={removeFromCart}>
+            <FcDeleteDatabase />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,60 +1,50 @@
-import React from "react";
-import { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import CartItem from "../components/CartItem"
+import CartItem from "../components/CartItem";
+import "./Cart.css";
 
 const Cart = () => {
+  const { cart } = useSelector((state) => state);
+  const [totalAmount, setTotalAmount] = useState(0);
 
-    const { cart } = useSelector((state) => state);
-const [totalAmount,setTotalAmount]=useState(0);
-useEffect( ()=>{
-    setTotalAmount(cart.reduce((acc,curr)=> acc+curr.price,0));
-},[cart])
+  useEffect(() => {
+    setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart]);
 
-
-    return (
+  return (
+    <div className="cart-container">
+      {cart.length > 0 ? (
         <div>
-            {
-                cart.length > 0 ?
-                    (<div>
-                        <div>
-                            {
-                                cart.map((item, index) => {
-                                    return <CartItem key={item.id} item={item} itemIndex={index} />
-                                })
-                            }
-                        </div>
-                        <div>
-
-                        <div>
-                            <div>Your Cart</div>
-                            <div>Summary</div>
-                            <p>
-                                <span>Total Items:{cart.length}</span>
-                            </p>
-                            </div>  
-                            <div>
-                                <p>Total Amount:${totalAmount}</p>
-                                <button>
-                                CheckOut Now
-                                </button>
-                                </div>              
-                        </div>
-
-
-                    </div>) :
-                    (<div>
-                        <h1>Cart Empty</h1>
-                        <Link to={"/"}>
-                            <button>
-                                Shop Now
-                            </button>
-                        </Link>
-                    </div>)
-            }
+          <div className="cart-items">
+            {cart.map((item, index) => (
+              <CartItem key={item.id} item={item} itemIndex={index} />
+            ))}
+          </div>
+          <div className="cart-summary">
+            <div>
+              <div className="cart-summary-title">Your Cart</div>
+              <div className="cart-summary-title">Summary</div>
+              <p>
+                <span>Total Items: {cart.length}</span>
+              </p>
+            </div>
+            <div className="cart-summary-details">
+              <p className="cart-summary-total">Total Amount: ${totalAmount}</p>
+              <button className="checkout-button">Check Out Now</button>
+            </div>
+          </div>
         </div>
-    );
+      ) : (
+        <div className="empty-cart">
+          <h1>Cart Empty</h1>
+          <Link to="/">
+            <button>Shop Now</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
